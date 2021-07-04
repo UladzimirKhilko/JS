c45c97cca118cbd0851244ee54703657b256ccc5
Vladimir <vladimirhilko@gmail.com>
2021-07-04 17:58:55 +0400

    part 2 - conditions and cycles

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько его оцените?', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+console.log(personalMovieDB);
+
+if (numberOfFilms < 10) {
+        console.log('Просмотренно довольно мало фильмов');
+    } else if (10 <= numberOfFilms && numberOfFilms < 30) {
+        console.log('Вы классический зритель');
+    } else if (numberOfFilms >= 30 ) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
